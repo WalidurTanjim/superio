bc41581b742836ae23cbacc09dc1a4c16dc6f843
WalidurTanjim <mohammadtanjim91@gmail.com>
2024-12-15 17:49:34 +0600

    404 error page added

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import './App.css'
 import MainLayout from './layout/MainLayout'
 import Home from './pages/Home/Home/Home'
 import FindJobs from './pages/FindJobs/FindJobs/FindJobs'
+import ErrorPage from './pages/ErrorPage/ErrorPage'
 
 function App() {
   const routes = createBrowserRouter([
-    {path: '/', element: <MainLayout />, children: [
+    {path: '/', errorElement: <ErrorPage />, element: <MainLayout />, children: [
       {path: '/', element: <Home />},
       {path: '/findJobs', element: <FindJobs />}
     ]}
