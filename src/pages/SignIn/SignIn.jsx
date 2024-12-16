f47c1d955b185b80e2eec16e0d8baca8009722f9
WalidurTanjim <mohammadtanjim91@gmail.com>
2024-12-16 17:09:08 +0600

    Show password added to signin & signup page

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -30,14 +30,14 @@ const SignIn = () => {
                         {/* password */}
                         <div className="w-full mb-3">
                             <label for="input-label" className="block text-sm text-slate-700 mb-1 dark:text-white">Password</label>
-                            <input type="text" id="input-label" className="py-2 px-4 block w-full outline-none border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Password" {...register("password", { required: true})} />
+                            <input type={showPassword ? 'text' : 'password'} id="input-label" className="py-2 px-4 block w-full outline-none border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Password" {...register("password", { required: true})} />
                         </div>
 
                         {/* show password & forgot password */}
                         <div className="mt-5 flex items-center justify-between text-xs">
                             {/* show password */}
                             <div className="flex gap-2">
-                                <input type="checkbox" name="checkbox" id="checkbox" />
+                                <input type="checkbox" name="checkbox" id="checkbox" onClick={() => setShowPassword(!showPassword)} />
                                 <p className='text-gray-500'>Show Password</p>
                             </div>
 
