52509be1736e92383d439dabc11112e2ec5b53b5
WalidurTanjim <mohammadtanjim91@gmail.com>
2024-12-18 01:26:51 +0600

    Job delete handler added & delete successfully with token verification

--- src/components/PostedJob/PostedJob.jsx
+++ src/components/PostedJob/PostedJob.jsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useAxiosPublic from '../../hooks/useAxiosPublic';
+import Swal from 'sweetalert2';
+import useAuth from '../../hooks/useAuth';
+import { useNavigate } from 'react-router-dom';
 
 const PostedJob = ({ job }) => {
     const { _id, company_name, company_logo, hr_email, title, category, job_type, job_category, salary } = job;
+
+    const [errMsg, setErrMsg] = useState('');
+    const { user } = useAuth();
+    const axiosPublic = useAxiosPublic();
+    const navigate = useNavigate();
+
+    // deleteHandler
+    const deleteHandler = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                try{
+                    const fetchData = async() => {
+                        const res = await axiosPublic.delete(`/myPostedJobs/${id}?email=${user?.email}`)
+                        // const res = await axiosPublic.delete(`/myPostedJobs/${id}?email=tanjim@gmail.com`)
+                        const data = await res.data;
+
+                        if(data?.deletedCount > 0){
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your job post has been deleted.",
+                                icon: "success"
+                            });
+                            navigate('/');
+                        }
+                    };
+                    fetchData();
+                }catch(err){
+                    setErrMsg(err?.response?.data?.message);
+                    console.error(err);
+                }
+            }
+        });
+    };
+
+    // updateHandler
+    const updateHandler = id => {
+        console.log("Update job id:", id);
+    }
 
     return (
         <tr>
@@ -50,14 +100,14 @@ const PostedJob = ({ job }) => {
             <td className="px-4 py-4 text-sm whitespace-nowrap">
                 <div className="flex items-center gap-x-6">
                     {/* delete btn */}
-                    <button className="text-gray-500 transition-colors duration-200 dark:hover:text-red-500 dark:text-gray-300 hover:text-red-500 focus:outline-none">
+                    <button className="text-gray-500 transition-colors duration-200 dark:hover:text-red-500 dark:text-gray-300 hover:text-red-500 focus:outline-none" onClick={() => deleteHandler(_id)}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                         </svg>
                     </button>
 
                     {/* edit btn */}
-                    <button className="text-gray-500 transition-colors duration-200 dark:hover:text-yellow-500 dark:text-gray-300 hover:text-yellow-500 focus:outline-none">
+                    <button className="text-gray-500 transition-colors duration-200 dark:hover:text-yellow-500 dark:text-gray-300 hover:text-yellow-500 focus:outline-none" onClick={() => updateHandler(_id)}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                         </svg>
---
+++ src/pages/ApplyJob/ApplyJob.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ApplyJob = () => {
+    return (
+        <section className='applyJob container mx-auto px-6 py-10'>
+            <h1>Apply job page</h1>
+        </section>
+    );
+};
+
+export default ApplyJob;
